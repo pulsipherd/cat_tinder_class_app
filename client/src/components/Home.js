b697414836189337d2131ade6badb204fd0203b9
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-21 14:11:38 -0600

    add styling, need work on swipe action

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import axios from 'axios';
 import '../cards.css';
-import Cards, { Card } from 'react swipe-card';
-import { image } from 'semantic-ui-react';
+import Cards, { Card } from 'react-swipe-card';
+import { Image } from 'semantic-ui-react';
 
 
 class Home extends Component {
@@ -13,12 +13,26 @@ class Home extends Component {
     axios.get('/api/cats')
       .then( res => this.setState({ cats: res.data }) )
   }
+  swipeLeft = (id) => {
+    let { cats } = this.state;
+    this.setState({ cats: cats.filter( c => c.id !== id ) })
+  }
+
+  swipeRight = (id) => {
+    let { cats } = this.state;
+    axios.put(`/api/cats/${id}`)
+      .then( () => this.setState({ cats: cats.filter( c => c.id !== id ) }) )
+  }
 
   render() {
     return (
       <Cards className="cards-root">
-        {this.state.cats.map(cat =>
-          <Card key={cat.id}>
+        {this.state.cats.map( cat =>
+          <Card
+            key={cat.id}>
+            onSwipeLeft={() => this.swipeLeft(cat.id) }
+            onSwipeRight={() => this.swipeRight(cat.id) }
+            >
             <h2>{cat.name}</h2>
             <Image src={cat.avatar} />
             <h3>{cat.breed}</h3>
